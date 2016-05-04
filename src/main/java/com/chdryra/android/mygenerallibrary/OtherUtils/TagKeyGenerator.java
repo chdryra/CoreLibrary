1139eacc161ae7c3f721679b7b8983370d19399f
Rizwan Choudrey <rizwan.choudrey@gmail.com>
2016-05-04 18:53:21 +0100

    Some code cleanup

---
+++ src/main/java/com/chdryra/android/mygenerallibrary/OtherUtils/TagKeyGenerator.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) Rizwan Choudrey 2016 - All Rights Reserved
+ * Unauthorized copying of this file via any medium is strictly prohibited
+ * Proprietary and confidential
+ * rizwan.choudrey@gmail.com
+ *
+ */
+
+package com.chdryra.android.mygenerallibrary.OtherUtils;
+
+/**
+ * Created by: Rizwan Choudrey
+ * On: 30/08/2015
+ * Email: rizwan.choudrey@gmail.com
+ */
+public class TagKeyGenerator {
+    public static String getTag(Class<?> clazz) {
+        return clazz.getName();
+    }
+
+    public static String getKey(Class<?> clazz, String key) {
+        return clazz.getPackage().getName() + "." +  clazz.getName() + "." + key;
+    }
+}
