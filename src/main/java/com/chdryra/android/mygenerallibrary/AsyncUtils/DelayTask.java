52a287828b34e58f5f89967902f2d8554eaa5b6d
Rizwan Choudrey <rizwan.choudrey@gmail.com>
2017-02-17 18:03:01 +0000

    Added a delay ro ReviewNodeRepo to stop it notifying listeners whilst subscriptions are being downloaded the first time.

---
+++ src/main/java/com/chdryra/android/mygenerallibrary/AsyncUtils/DelayTask.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) Rizwan Choudrey 2017 - All Rights Reserved
+ * Unauthorized copying of this file via any medium is strictly prohibited
+ * Proprietary and confidential
+ * rizwan.choudrey@gmail.com
+ *
+ */
+
+package com.chdryra.android.mygenerallibrary.AsyncUtils;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by: Rizwan Choudrey
+ * On: 17/02/2017
+ * Email: rizwan.choudrey@gmail.com
+ */
+
+public class DelayTask extends AsyncTask<Long, Void, Void> {
+    @Override
+    protected Void doInBackground(Long... delay) {
+        try {
+            Thread.sleep(delay[0]);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
+        return null;
+    }
+}
