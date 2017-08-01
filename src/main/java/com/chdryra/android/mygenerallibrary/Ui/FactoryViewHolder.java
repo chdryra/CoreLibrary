0fa5fa490476c465fe29ec8a2ab19e838eec3025
Rizwan Choudrey <rizwan.choudrey@gmail.com>
2017-08-01 16:06:02 +0100

    In the process of moving to material design. Changed GridView to RecyclerView

---
+++ src/main/java/com/chdryra/android/mygenerallibrary/Ui/FactoryViewHolder.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) Rizwan Choudrey 2017 - All Rights Reserved
+ * Unauthorized copying of this file via any medium is strictly prohibited
+ * Proprietary and confidential
+ * rizwan.choudrey@gmail.com
+ *
+ */
+
+package com.chdryra.android.mygenerallibrary.Ui;
+
+import android.view.View;
+
+/**
+ * Created by: Rizwan Choudrey
+ * On: 02/07/2017
+ * Email: rizwan.choudrey@gmail.com
+ */
+
+public interface FactoryViewHolder<DataType> {
+    ViewHolderAbstract<DataType> newViewHolder(View v, int position);
+
+    int getViewHolderLayout(int viewType);
+}
