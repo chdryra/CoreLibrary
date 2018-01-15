792434817bd21ea3708d30cf60c4ad7a3f51da9b
Rizwan Choudrey <rizwan.choudrey@gmail.com>
2018-01-15 18:58:15 +0000

    Some UI tweaking

--- src/main/java/com/chdryra/android/mygenerallibrary/Ui/GridItemDecoration.java
+++ src/main/java/com/chdryra/android/mygenerallibrary/Ui/GridItemDecoration.java
@@ -34,23 +34,18 @@ public class GridItemDecoration extends RecyclerView.ItemDecoration {
 
     @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-        int position = parent.getChildAdapterPosition(view); // item position
-        int column = position % spanCount; // item column
+        int position = parent.getChildAdapterPosition(view);
+        int column = position % spanCount;
 
         if (includeEdge) {
-            outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
-            outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
-
-            if (position < spanCount) { // top edge
-                outRect.top = spacing;
-            }
-            outRect.bottom = spacing; // item bottom
+            outRect.left = spacing - column * spacing / spanCount;
+            outRect.right = (column + 1) * spacing / spanCount;
+            outRect.bottom = spacing;
+            if (position < spanCount) outRect.top = spacing;
         } else {
-            outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
-            outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
-            if (position >= spanCount) {
-                outRect.top = spacing; // item top
-            }
+            outRect.left = column * 2* spacing / spanCount;
+            outRect.right = spacing - 2 * (column + 1) * spacing / spanCount;
+            if (position >= spanCount) outRect.top = spacing;
         }
     }
 }
